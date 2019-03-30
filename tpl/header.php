b0e2c2abedf760639c95e9b89fb03915e576ef90
Viktor Sidoretc <silrage@MacBook-Pro-Viktor.local>
2019-03-30 10:46:27 +0300

    Init templates structure
    +Add template #header

---
+++ tpl/header.php
@@ -0,0 +1,31 @@
+  <header>
+    <div class="container space-top">
+      <div class="site-title">
+        <h1 style="font-family: Times New Roman;">Храм Тихвинской иконы Божией Матери</h1>  
+      </div>
+      <div class="site-desc">
+        <p>Патриаршее подворье Симонова онастыря г. Москвы</p>
+      </div>
+      <div class="site-primary-menu">
+        <div class="site-tabs">
+          <div class="site-tabs__wrapper">
+            <a href="#" class="site-tabs__item is-active">Новости</a>
+            <a href="#" class="site-tabs__item">Храм</a>
+            <a href="#" class="site-tabs__item">Духовенство</a>
+            <a href="#" class="site-tabs__item">История Храма</a>
+            <a href="#" class="site-tabs__item">История монастыря</a>
+            <a href="#" class="site-tabs__item">Святые и святыни Монастыря</a>
+            <a href="#" class="site-tabs__item">Помощь Храму</a>
+            <a href="#" class="site-tabs__item">Расписание Богослужений</a>
+            <a href="#" class="site-tabs__item">Медиа</a>
+            <a href="#" class="site-tabs__item">Контакты</a>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div class="site-lang">
+      <a href="#" class="is-active">Русский</a>
+      <a href="#">English</a>
+    </div>
+  </header>
---
+++ tpl/index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+
+  <title>Tihchurch</title>
+
+  <link href="./css/global.css" rel="stylesheet" />
+
+</head>
+<body>
+
+  <main id="site-main">
+    <div class="site-page-content">
+      <?php include './header.php';?>
+
+    </div>
+  </main>
+
+</body>
+</html>
