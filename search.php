07f63e06275d9839652afebc30f0881f46bbb575
Horuzhenko Alexander <horuzhenko.alex@gmail.com>
2019-04-04 11:53:06 +0300

    убрал сползание влево

--- search.php
+++ search.php
@@ -1,7 +1,7 @@
 <?php get_header(); ?>
 
-<section class="content">
-    <div class="container row">
+<section class="content container">
+    <div class="row">
         <div class="news-container col-9 col-td-6">
 
             <h1><?php echo sprintf( __( '%s Search Results for ', 'html5blank' ), $wp_query->found_posts ); echo get_search_query(); ?></h1>
